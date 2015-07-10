7f43b4e45934fcd2902677c09692a6e7580005be
Carl Byström <cgbystrom@gmail.com>
2015-07-11 00:34:31 +0200

    Removed nulls
    Will have to figure out a good way to handle empty description/descriptionReason. But for now, just avoiding null is good enough.

--- src/main/scala/se/uprise/graphql/types/GraphQLMetadata.scala
+++ src/main/scala/se/uprise/graphql/types/GraphQLMetadata.scala
@@ -2,6 +2,6 @@ package se.uprise.graphql.types
 
 trait GraphQLMetadata {
   def name: String
-  def description: String = null
-  def deprecationReason: String = null
+  def description: String = ""
+  def deprecationReason: String = ""
 }
