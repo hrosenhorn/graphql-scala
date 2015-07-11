39fdb7a0aae34d6eaeb97f83de0f8b7691b26e15
Håkan Rosenhorn <hrosenhorn@ea.com>
2015-07-11 12:31:47 +0200

    Removed file template comment and defualt to None instead of null

--- src/main/scala/se/uprise/graphql/types/GraphQLSchema.scala
+++ src/main/scala/se/uprise/graphql/types/GraphQLSchema.scala
@@ -1,6 +1,3 @@
 package se.uprise.graphql.types
 
-/**
- * Created by Håkan Rosenhorn on 2015-07-09.
- */
-class GraphQLSchema(query: GraphQLObjectType, mutation: Option[GraphQLObjectType] = null)
+class GraphQLSchema(query: GraphQLObjectType, mutation: Option[GraphQLObjectType] = None)
